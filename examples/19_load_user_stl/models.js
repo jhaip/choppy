873255a570110eb012064c31628bd6a6ff25e4fb
Jacob Haip <jhaip@mit.edu>
2017-08-10 21:31:38 -0400

    load user submitted STLs through file upload

---
+++ examples/19_load_user_stl/models.js
@@ -0,0 +1,29 @@
+function loadSTL() {
+  var loader = new THREE.STLLoader();
+
+  // var model_path = '../../models/stl/binary/JuliaVase001_-_Aqua_-_2mm.stl';
+  var model_path = '../../models/stl/binary/dino.stl';
+
+  loader.load( model_path, function ( geometry ) {
+    var geometry2 = new THREE.Geometry().fromBufferGeometry( geometry );
+    var mesh = new THREE.Mesh( geometry2, material );
+    mesh.position.set(0,0,-0.5);
+    mesh.scale.set( .01, .01, .01 );
+    addMesh(mesh);
+    render();
+    enterIdle();
+  });
+}
+
+function loadUserSTL(data_uri) {
+  var loader = new THREE.STLLoader();
+  loader.load( data_uri, function ( geometry ) {
+    var geometry2 = new THREE.Geometry().fromBufferGeometry( geometry );
+    var mesh = new THREE.Mesh( geometry2, material );
+    mesh.position.set(0,0,-0.5);
+    mesh.scale.set( .01, .01, .01 );
+    addMesh(mesh);
+    render();
+    enterIdle();
+  });
+}
---
+++ examples/19_load_user_stl/state.js
@@ -0,0 +1,89 @@
+/* EMPTY
+   LOADING_MODEL
+   IDLE
+   EDIT_CUT_PLANE
+   ADD_KEY
+*/
+var scene_store = {
+  "state": "EMPTY",
+  "models": [],
+  "cut_plane": null
+};
+setState("EMPTY");
+
+function setState(state) {
+  scene_store.state = state;
+  $(".sidebar__current-state").text("State: "+state);
+  console.log("UPDATED STATE: " + state);
+  updateUIControls();
+  updatePartListDisplay();
+}
+
+function enterIdle() {
+  hideCutPlane();
+  for (var i=0; i<scene_store.models.length; i+=1) {
+    scene_store.models[i].mesh.material = material.clone();
+    scene_store.models[i].activecut = false;
+  }
+  sphere_raycast.visible = false;
+  render();
+  setState("IDLE");
+  hideTransformControls();
+}
+
+function enterEditCutPlane() {
+  setState("EDIT_CUT_PLANE");
+  showCutPlane();
+  showTransformControls();
+}
+
+function enterAddKeyState() {
+  setState("ADD_KEY");
+  showCutPlane();
+  hideTransformControls();
+  toggleMeshOpacity(0);
+  toggleMeshOpacity(1);
+  sphere_raycast.visible = true;
+  render();
+}
+
+function addMesh(mesh, opts) {
+  console.log("------> add mesh");
+  var model_opts = {
+    "id": 1,
+    "visible": true,
+    "selected": false,
+    "activecut": false,
+    "color": 0x00ff00,
+    "mesh": mesh
+  };
+  if (opts !== undefined) {
+    if (opts.activecut) {
+      model_opts.activecut = opts.activecut
+    }
+  }
+  scene_store.models.push(model_opts);
+  scene.add( mesh );
+
+  updatePartListDisplay();
+
+  return model_opts;
+}
+
+function removeMesh(mesh) {
+  console.log("<------ removing mesh");
+  for (var i=0; i<scene_store.models.length; i+=1) {
+    if (scene_store.models[i].mesh === mesh) {
+      console.log("found it");
+      console.log(scene_store.models.length);
+      // new_models = scene_store.models.splice(i, 1);
+      scene_store.models.splice(i, 1);
+      scene.remove( mesh );
+      updatePartListDisplay();
+      return;
+    }
+  }
+  console.error("mesh not found!");
+  console.error(scene_store.models);
+  console.error(mesh)
+}
